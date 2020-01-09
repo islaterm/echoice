23a7a9993034fdb09bd97eac5803ba63a9dfd464
Ignacio Slater Muñoz <ignacio.slater@ug.uchile.cl>
2020-01-08 22:46:51 -0300

    Created tests for link parsing v1.0.1.4

--- src/main/kotlin/ecchoice/scrapping/EHFetcher.kt
+++ src/main/kotlin/ecchoice/scrapping/EHFetcher.kt
@@ -6,12 +6,20 @@ package ecchoice.scrapping
  * For information on the API's usage refer to [ehwiki](https://ehwiki.org/wiki/API).
  *
  * @author [Ignacio Slater Muñoz](islaterm@gmail.com)
- * @version 1.0.1.3
+ * @version 1.0.1.4
  * @since 1.0
  */
 class EHFetcher {
   fun getGallery(link: String): EHGallery {
     return EHGallery(link, "", "", setOf())
+  }
+
+  /**
+   * Parses the given ``url`` and returns a ``Pair<Long, String>`` containing the
+   * _gallery id_ and the _gallery token_ respectively.
+   */
+  fun parseURL(url: String): Pair<Long, String> {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
   }
 }
 
--- src/test/kotlin/ecchoice/entities/EHGalleryTest.kt
+++ src/test/kotlin/ecchoice/entities/EHGalleryTest.kt
@@ -5,24 +5,12 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 private const val GALLERY_LINK = "https://e-hentai.org/g/618395/0439fa3666/"
-private const val GALLERY_TITLE = "(Kouroumu 8) [Handful☆Happiness! (Fuyuki Nanahara)] " +
-    "TOUHOU GUNMANIA A2 (Touhou Project)"
-private const val GALLERY_THUMBNAIL_URL = "https://ehgt.org/14/63/" +
-    "1463dfbc16847c9ebef92c46a90e21ca881b2a12-1729712-4271-6032-jpg_l.jpg"
-private val GALLERY_TAGS = setOf(
-  "Non-H",
-  "parody:touhou project",
-  "group:handful happiness",
-  "artist:nanahara fuyuki",
-  "full color",
-  "artbook"
-)
 
 /**
  * Test set for the functionalities of the ``EHGallery`` class.
  *
  * @author [Ignacio Slater Muñoz](islaterm@gmail.com)
- * @version 1.0.1.3
+ * @version 1.0.1.4
  * @since 1.0
  */
 internal class EHGalleryTest {
@@ -39,14 +27,42 @@ internal class EHGalleryTest {
   }
 
   /**
+   * Retrieves the _gallery id_ and _gallery token_ from the link defined in
+   * [GALLERY_LINK].
+   */
+  @Test
+  fun parseLinkTest() {
+    val idTokenPair = testFetcher.parseURL(GALLERY_LINK)
+    assertEquals(Pair(618395, "0439fa3666"), idTokenPair)
+  }
+
+  /**
    * Tests that the fetcher can obtain the gallery data correctly.
    */
   @Test
   fun testGalleryFetch() {
     val testEHGallery = testFetcher.getGallery(GALLERY_LINK)
     assertEquals(GALLERY_LINK, testEHGallery.link)
-    assertEquals(GALLERY_TITLE, testEHGallery.title)
-    assertEquals(GALLERY_THUMBNAIL_URL, testEHGallery.thumbnailURL)
-    assertEquals(GALLERY_TAGS, testEHGallery.tags)
+    assertEquals(
+      "(Kouroumu 8) [Handful☆Happiness! (Fuyuki Nanahara)] " +
+          "TOUHOU GUNMANIA A2 (Touhou Project)",
+      testEHGallery.title
+    )
+    assertEquals(
+      "https://ehgt.org/14/63/" +
+          "1463dfbc16847c9ebef92c46a90e21ca881b2a12-1729712-4271-6032-jpg_l.jpg",
+      testEHGallery.thumbnailURL
+    )
+    assertEquals(
+      setOf(
+        "Non-H",
+        "parody:touhou project",
+        "group:handful happiness",
+        "artist:nanahara fuyuki",
+        "full color",
+        "artbook"
+      ), testEHGallery.tags
+    )
   }
+
 }
